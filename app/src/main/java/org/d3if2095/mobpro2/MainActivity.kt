3ba7e124f5a8650e49fb0d115088109c3c6354c6
Arif Mukhna Arafah <arifmukh@student.telkomuniversity.ac.id>
2020-09-14 13:04:31 +0700

    Initial commit

---
+++ app/src/main/java/org/d3if2095/mobpro2/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.d3if2095.mobpro2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
